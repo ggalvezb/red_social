0cca1916e31174c05d2f5f097b52f069e4a2da2d
ggalvezb <32131902+ggalvezb@users.noreply.github.com>
2020-03-03 20:29:41 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const config=require('./server/config');
+
+//Database
+require('./database');
+
+//Starting server
+const app=config(express());
+
+
+app.listen(app.get('port'), () =>{
+    console.log('Server on port', app.get('port'));
+})
